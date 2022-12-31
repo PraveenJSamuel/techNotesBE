0d1cd8917f536830bb319ed4d7d1a2be8405853c
Praveen Samuel <prsamuel1994@gmail.com>
2022-12-31 17:05:05 -0500

    backend production build

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'https://technotes-yv1l.onrender.com',
+    'https://www.technotes-yv1l.onrender.com',
+    'https://techrepairshop.com',
+]
+
+module.exports = allowedOrigins
